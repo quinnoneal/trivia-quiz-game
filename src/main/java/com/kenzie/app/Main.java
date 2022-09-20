638f8721370bfd043bc86a0ab9538532755e68b7
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2022-09-19 18:18:32 -0700

    cleaned up code.

--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -3,8 +3,10 @@ package com.kenzie.app;
 // import necessary libraries
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kenzie.app.gamedto.Clues;
 import com.kenzie.app.gamedto.GameDTO;
 
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -17,19 +19,20 @@ public class Main {
         GameDTO gameDTO;
         ObjectMapper mapper = new ObjectMapper();
         Random r = new Random();
-
+        List<Clues> cluesList;
         try {
             // get list of questions with client
             String response = CustomHttpClient.sendGET(URL);
             gameDTO = mapper.readValue(response, GameDTO.class);
+            cluesList = gameDTO.getClues();
             // start game
             for (int i = 0; i < 10; i++) {
                 // ask user a random question
                 int randNum = r.nextInt(100);
                 // retrieve category, question, and answer from random index
-                String category = gameDTO.getClues().get(randNum).getCategory().getTitle();
-                String question = gameDTO.getClues().get(randNum).getQuestion();
-                String answer = gameDTO.getClues().get(randNum).getAnswer();
+                String category = cluesList.get(randNum).getCategory().getTitle();
+                String question = cluesList.get(randNum).getQuestion();
+                String answer = cluesList.get(randNum).getAnswer();
                 System.out.println("Category: " + category);
                 System.out.println("Question: " + question);
                 // take in user input
